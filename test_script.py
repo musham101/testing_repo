4f3ab0fd2b4c827549f56876c8bfcc5ef6771306
Musham Malik <mushammalik@Mushams-MacBook-Pro.local>
2023-03-06 15:29:56 +0500

    first commit

---
+++ test_script.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def load_data(file_path):
+    data = pd.read_csv(file_path)
+    return data
+
+def print_head(data, n):
+    return data.head(n)
+
+file_path = "annual-enterprise-survey-2021-financial-year-provisional-csv.csv"
+
+data = load_data(file_path)
+
+print(print_head(data, 5))
